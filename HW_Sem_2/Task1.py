dc5a5e9cb211aa1e3a826b99d879801414bad0a6
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-10-26 12:01:32 +0300

    Add srings in HW_Sem_2

--- HW_Sem_2/Task1.py
+++ HW_Sem_2/Task1.py
@@ -3,13 +3,18 @@
 # Пример:
 # 6782 -> 23
 # 0,56 -> 11
-num = input("Введите число: ")
-num1 = num
-if '.' in num:
-    num1 = num.replace('.', '')
-elif ',' in num:
-    num1 = num.replace(',', '')
-sum_nums = 0
-for i in num1:
-    sum_nums += int(i)
-print(f'Введённое число: {num} сумма чисел данного числа: {sum_nums}')
+
+# num = input("Введите число: ")
+# num1 = num
+# if '.' in num:
+#     num1 = num.replace('.', '')
+# elif ',' in num:
+#     num1 = num.replace(',', '')
+# sum_nums = 0
+# for i in num1:
+#     sum_nums += int(i)
+# print(f'Введённое число: {num} сумма чисел данного числа: {sum_nums}')
+
+#Вариант 2
+print(sum(map(int, (c for c in input('Введите число: ') if c.isdigit()))))
+# or isdecimal()
--- HW_Sem_2/Task5.py
+++ HW_Sem_2/Task5.py
@@ -1,10 +1,18 @@
 # Реализуйте алгоритм перемешивания списка.
-import numbers
-from random import randint
-a = int(input('Введите число обозначающее количество чисел в списке:'))
-num_list = [i for i in range(a)]
-print(f"Первоначальный список: {num_list}")
-for i in range(len(num_list)):
-    rand = randint(0, a)
-    num_list[i], num_list[rand] = num_list[rand], num_list[i]
-print(f"Перемешанный список: {num_list}")
+# import numbers
+# from random import randint
+# a = int(input('Введите число обозначающее количество чисел в списке:'))
+# num_list = [i for i in range(a)]
+# print(f"Первоначальный список: {num_list}")
+# for i in range(len(num_list)):
+#     rand = randint(0, a)
+#     num_list[i], num_list[rand] = num_list[rand], num_list[i]
+# print(f"Перемешанный список: {num_list}")
+
+# ВАРИК 2
+import random
+n = int(input("Длина списка: "))
+lst = list(range(n))
+print(f"Первоначальный список: {lst}")
+random.shuffle(lst)
+print(lst)
